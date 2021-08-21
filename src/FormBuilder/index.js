a651337703226272222ff70d4a9bd38eb6968c3d
Andrii Zakopets <snailz839@gmail.com>
2021-08-21 18:02:44 +0300

    add typescript

--- src/FormBuilder/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FormBuilder() {
-  return <div>Hello World!</div>;
-}
-
-export default FormBuilder;
---
+++ src/FormBuilder/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function FormBuilder(): React.ReactElement {
+  return <div>Hello World!</div>;
+}
+
+export default FormBuilder;
