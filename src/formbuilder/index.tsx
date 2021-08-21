fedce53e081c0009898909fed0f5fcc56554fdc4
Andrii Zakopets <snailz839@gmail.com>
2021-08-21 17:00:22 +0300

    init

---
+++ src/formbuilder/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function FormBuilder() {
+  return <div>Hello World!</div>;
+}
+
+export default FormBuilder;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import FormBuilder from './formbuilder';
+
+export {
+  FormBuilder
+};
+
+export default FormBuilder;
